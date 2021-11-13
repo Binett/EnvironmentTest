ce30cb999c73fe212cd19c77302387b3a6624c1d
Binett <73723725+Binett@users.noreply.github.com>
2021-11-13 01:14:04 +0100

    Test

--- EnvironmentTest/Program.cs
+++ EnvironmentTest/Program.cs
@@ -6,7 +6,7 @@ namespace EnvironmentTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World1");
+            Console.WriteLine("Hello JA");
         }
     }
 }
